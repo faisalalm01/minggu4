cf2f32df0a4b72f360b5e7b2df82c81a23771bbb
faisalalm01 <faisalali2858@gmail.com>
2021-05-25 10:30:26 +0700

    maintenane

--- src/routes/foodsRoutes.js
+++ src/routes/foodsRoutes.js
@@ -2,8 +2,10 @@ const foodsRoutes = require ("express").Router();
 const foodsControllers = require("../controllers/foodsControllers")
 const authMiddleware = require("../helpers/authMiddleware");
 
-foodsRoutes.get("/", authMiddleware.checkLogin,foodsControllers.getAllFoods);
-foodsRoutes.post("/",foodsControllers.postFoods);
+foodsRoutes.get("/", 
+// authMiddleware.checkLogin,
+foodsControllers.getAllFoods);
+foodsRoutes.post("/",authMiddleware.checkLogin, foodsControllers.postFoods);
 foodsRoutes.get("/:id",foodsControllers.getDataById);
 foodsRoutes.delete("/:id",foodsControllers.deleteDataById);
 foodsRoutes.put("/:id",foodsControllers.putDataById);
