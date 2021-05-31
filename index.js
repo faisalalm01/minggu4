9f8b48bb72e30fee9b7e56d255108cffbdb309c0
faisalalm01 <faisalali2858@gmail.com>
2021-05-31 18:19:45 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config({})
 const express = require("express")
 const app = express();
-const port = process.env.PORT || 3001
 const cors = require('cors')
+const port = process.env.PORT || 3001
 
 const mainRoutes = require('./src/routes/');
+app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use('/', mainRoutes)
-app.use(cors())
 
 
 app.listen(port, ()=>{
